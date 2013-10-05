a35b296d02f0998277c2e89333108d2b8163f3f4
Reno Qiu <dechao.qiu@gmail.com>
2013-10-05 21:21:22 +0800

    Site updated at 2013-10-05 13:21:22 UTC

--- downloads/code/SMTP.py
+++ downloads/code/SMTP.py
@@ -4,7 +4,7 @@ import ssl
 import base64
 import getpass
 
-msg="\r\n I love computer networks!"
+msg="\r\n I love Computer Communication Networks!"
 endmsg="\r\n.\r\n"
 
 # choose a mail server (e.g. a Google server) and call it mailserver
@@ -14,7 +14,7 @@ print "Gmail requires your user account to send mail."
 username = raw_input("Your Gmail Address: ")
 print "HINT: When you input your password, it will not echo back..."
 password = getpass.getpass()
-# create socket called clientSocket and establish a TCP connection with mailserver
+
 clientSocket = None
 try:
     clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -22,8 +22,8 @@ except error, msg:
     print 'Create Socket Error. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
     sys.exit();
 
-
-#Fill in start  
+print "Creating Socket Succeeded."
+print "About to get %s's ip address." % mailserver
 try:
     remote_ip = gethostbyname(mailserver)
 except gaierror:
@@ -31,140 +31,101 @@ except gaierror:
     print "Coule Not Find Server: %s. Exiting..." % mailserver
     sys.exit()
 
-#print 'Ip address of ' + mailserver + ' is ' + remote_ip
+print 'Ip address of ' + mailserver + ' is ' + remote_ip
 
 port = 587
-
 clientSocket.connect((remote_ip , port))
 print 'Socket Connect Successed...'
 
-#Fill in end
-
 recv=clientSocket.recv(1024)
 
-print recv
-
-if recv[:3]!='220':
+if recv[:3] != '220':
     print '220 reply not received from server.'
 
-
+print "Server:\t%s" % recv
 #Send HELO command and print server response.
 
 heloCommand='HELO Alice\r\n'
-
-
+print 'Client:\t%s' % heloCommand
 clientSocket.send(heloCommand)
 
 recv1=clientSocket.recv(1024)
 
-print recv1
-
 if recv1[:3]!='250':
     print '250 reply not received from server.'
 
+print "Server:\t%s" % recv1
 #Send MAIL FROM command and print server response.
 
-#Fill in start
 command = "STARTTLS\r\n"
-print "STARTTLS"
+print "Client:\tSTARTTLS"
 clientSocket.send(command)
 recvdiscard = clientSocket.recv(1024)
-print recvdiscard
+print "Server:\t%s" % recvdiscard
 
 clientSocket = ssl.wrap_socket(clientSocket, ssl_version=ssl.PROTOCOL_SSLv23)
 
 authCommand='AUTH LOGIN\r\n'
-print "AUTH LOGIN"
+print "Client:\t%s" % authCommand
 clientSocket.send(authCommand)
 recv1=clientSocket.recv(1024)
-print recv1
+print "Server:\t%s" % recv1
 
 loginCommand = base64.b64encode(username)+'\r\n'
+print "Client:\t%s" % loginCommand
 clientSocket.send(loginCommand)
 recv1=clientSocket.recv(1024)
-print recv1
+print "Server:\t%s" % recv1
 
 loginCommand = base64.b64encode(password)+'\r\n'
+print "Client:\t%s" % loginCommand
 clientSocket.send(loginCommand)
 recv1=clientSocket.recv(1024)
-print recv1
+print "Server:\t%s" % recv1
 
 command = "MAIL FROM: <%s>\r\n" % username
+print "Client:\t%s" % command
 clientSocket.send(command)
-print command
 recv1 = clientSocket.recv(1024)
-print recv1
-
-#Fill in end
-
+print "Server:\t%s" % recv1
 
 #Send RCPT TO command and print server response.
 
 #Fill in start
-dest_email = "734589640@qq.com"
+dest_email = "me@renoqiu.com"
 command = "RCPT TO: <%s>\r\n" % dest_email
+print "Client:\t%s" % command
 clientSocket.send(command)
-print command
 recv1 = clientSocket.recv(1024)
-print recv1
-
-#Fill in end
-
+print "Server:\t%s" % recv1
 
 #Send DATA command and print server response.
 
-#Fill in start
 command = "DATA\r\n"
+print "Client:\t%s" % command
 clientSocket.send(command)
-print command
 recv1 = clientSocket.recv(1024)
-print recv1
-
-#Fill in end
-
+print "Server:\t%s" % recv1
 
 #send message data.
 
-#Fill in start
-SUBJECT = "From Han Tang"
+SUBJECT = "From Dechao Qiu"
 TEXT = msg + endmsg
 message = """\
 From: %s
 To: %s
 Subject: %s
-
 %s
 """ % (username, dest_email, SUBJECT, TEXT)
+print "Client:\t%s" % message
 clientSocket.send(message)
-print command
 recv1 = clientSocket.recv(1024)
-print recv1
-
-
-#Fill in end
-
-
-#message ends with a single period.
-
-command = ".\r\n"
-clientSocket.send(command)
-print command
-recv1 = clientSocket.recv(1024)
-print recv1
-
-#Fill in start
-
-
-#Fill in end
-
+print "Server:\t%s" % recv1
 
 #send QUIT command and get server response.
 
-#Fill in start
 command = "QUIT\r\n"
+print "Client:\t%s" % command
 clientSocket.send(command)
-print command
 recv1 = clientSocket.recv(1024)
-print recv1
-
-#Fill in end
+print "Server:\t%s" % recv1
